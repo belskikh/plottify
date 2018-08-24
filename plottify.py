d0af544b2e4e792752cf527ce065530bd58a3edb
Aleksandr <belskikh.aleksandr@gmail.com>
2018-08-24 14:21:48 +0300

    rle transformation funcs

--- plottify.py
+++ plottify.py
@@ -108,6 +108,40 @@ def plot_grid(objects, plot_func, ncols=4, fig_width=18):
     plt.tight_layout()
     plt.show()
 
+
+##############################
+####### RLE FUNCTIONS ########
+##############################
+
+def rle_from_masked(img):
+    shape = img.shape
+    RED = 0
+    mask = (img[:,:,RED] >= 254).astype(np.int)
+    is_line = False
+    idx = None
+    pixels = None
+    rle = []
+    for i, elem in enumerate(mask.reshape(-1)):
+        if elem == 1:
+            if not is_line:
+                is_line = True
+                idx = i
+                pixels = 0
+            pixels += 1
+        else:
+            if is_line:
+                is_line = False
+                rle.append((idx, pixels))
+    return rle
+
+
+def mask_from_rle(rle, imshape):
+    mask = np.zeros(imshape[0] * imshape[1], dtype=np.uint8)
+    for start, length in rle:
+        mask[start: start+length] = 1
+    return mask.reshape(imshape[:2])
+
+
 ##############################
 ####### HELP FUNCTIONS #######
 ##############################
